f97fc903419eff579953944a7636c1f752b8de79
GamBit1986 <52270313+GamBit1986@users.noreply.github.com>
2022-04-19 16:57:33 +0500

    Add files via upload

--- task_4_2.py
+++ task_4_2.py
@@ -1,33 +1,83 @@
 import requests
-from bs4 import BeautifulSoup
-from datetime import datetime as dt
-import json
-import re
-from re import sub
-from decimal import Decimal
-import io
 from datetime import datetime
-import pandas as pd
-i = 0
-list_temp_sec = []
 
 def currency_rates(code: str) -> float:
     """возвращает курс валюты `code` по отношению к рублю"""
+    list_temp_sec = []
+    list_temp_third = []
+    money = []
 
+    list_temp_val_2 = []
+    list_temp_val_3 = []
+    value = []
+
+    # result = []
+
+    code = code.upper()
+    print(code)
     r = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
-    print(type(r))
-    print(r)
+
+
+    # Эта часть кода отрезает списки по названия валют
+
     list_temp = r.split("</CharCode>")
-    print(list_temp)
-    for i in list_temp:
-        list_temp_sec.append(i.split("<"))
-    print(list_temp_sec)
-    for i in list
-    # print(convert)
 
-    result_value = 1.11  ## здесь должно оказаться результирующее значение float
-    return result_value
+    for k in list_temp:
+        list_temp_sec.append(k.split("<"))
+
+    i = 0
+    while i < len(list_temp_sec)-1:
+        list_temp_third.append(list_temp_sec[i][:1:-1][:1:])
+        i += 1
+
+    i = 0
+    for elem in list_temp_third:
+        money.append(elem[i][::-1][:3][::-1])
+
+    # Здесь делаем то же самое с курсовой стоимостью
+
+    list_temp_val = r.split("</Value>")
+    for k in list_temp_val:
+        list_temp_val_2.append(k.split("<"))
+
+    i = 0
+    while i < len(list_temp_val_2)-1:
+        list_temp_val_3.append(list_temp_val_2[i][:1:-1][:1:])
+        i += 1
+
+    i = 0
+    for elem in list_temp_val_3:
+        value.append(elem[i].strip('Value>'))
+
+    # Проверяем на совпадение, если найдено, то выводим значение, иначе- возвращаем None
+
+    i = 0
+    for mon in money:
+        i += 1
+        if mon == code:
+            result_value = float(value[i-1].replace(',', '.'))
+            break
+        else:
+            result_value = None
+
+    # Находим дату и конвертируем в формат datetime
+
+    temp_date = (((r.split(" name")[:1])[0])[::-1][:11][::-1]).strip('"')
+    r = map(int, temp_date.split('.'))
+    print(type(r[0]))
+    # year, month, day = map(int, temp_date.split('.'))
+    # dt_start = datetime.date(year, month, day)
+    # print(dt_start)
+    # today = datetime.strptime(temp_date, "%d.%m.%Y")
+    # t = today.timetuple()
+    # print(today)
+    result = 1
+    # result = (result_value, datetime.strftime(today))
+
+    return result
 
 
-print(currency_rates("USD"))
-# print(currency_rates("noname"))
+print(currency_rates("USD"), type(currency_rates("USD")))
+print(currency_rates("noname"))
+print(currency_rates("eur"))
+print(currency_rates("sek"))
