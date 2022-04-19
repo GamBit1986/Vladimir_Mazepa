94cf6f824fa19751b465402dce370087a78d9d8f
GamBit1986 <52270313+GamBit1986@users.noreply.github.com>
2022-04-19 20:10:50 +0500

    Файлы по 4 заданию

---
+++ task_4_4.py
@@ -0,0 +1,28 @@
+import utils
+
+print(utils.currency_rates("EUR"))
+print(utils.currency_rates("USD"))
+print(utils.currency_rates("tmt"))
+print(utils.currency_rates("UZS"))
+
+"""
+C:\python_projects\lesson_4\venv\Scripts\python.exe C:/python_projects/lesson_4/task_4_4.py
+EUR
+2022-04-20 00:00:00 <class 'datetime.datetime'>
+85.9674
+0
+USD
+2022-04-20 00:00:00 <class 'datetime.datetime'>
+79.0287
+0
+TMT
+2022-04-20 00:00:00 <class 'datetime.datetime'>
+22.5796
+0
+UZS
+2022-04-20 00:00:00 <class 'datetime.datetime'>
+70.0732
+0
+
+Process finished with exit code 0
+"""
