dc327f8e9a892f59d919a1365f74bb2e7b48a799
GamBit1986 <52270313+GamBit1986@users.noreply.github.com>
2022-04-22 21:31:45 +0500

    Add files via upload

--- task_5_3.py
+++ task_5_3.py
@@ -1,21 +1,26 @@
 tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
 klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '11А' ]
-
+i = 0
 
 def check_gen(tutors: list, klasses: list):
-    gen_tutors = (tutors[i] for i in range(len(tutors)))
-    gen_klasses = (klasses[i] for i in range(len(klasses)))
+    # Проверку можно усложнить, чтобы проверялось сколько именно элементов с None добавить, но в условии этого не было
+    if len(tutors) < len(klasses):
+        tutors.append(None)
+    elif len(tutors) == len(klasses):
+        pass
+    else:
+        klasses.append(None)
+    gen_tutors = ((tutors[i], klasses[i]) for i in range(len(tutors)))
     yield gen_tutors
-     # yield gen_klasses
+
 
 
 
 generator = check_gen(tutors, klasses)
-# for i in generator:
-#     print(*i, type(i))
-# print(type(generator))
 
 # добавьте здесь доказательство, что создали именно генератор
-for _ in range(len(tutors)):
-    print(*tuple(next(generator)))
+
+print(*next(generator), sep='\n')
+print(type(generator))
+
 # next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
---
+++ task_5_5.py
@@ -0,0 +1,17 @@
+def get_uniq_numbers(src: list):
+    unique_nums = set()
+    tmp = set()
+    for el in src:
+        if el not in tmp:
+            unique_nums.add(el)
+        else:
+            unique_nums.discard(el)
+        tmp.add(el)
+
+    unique_nums_ord = [el for el in src if el in unique_nums]
+
+    return unique_nums_ord
+
+
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+print(*get_uniq_numbers(src), type(get_uniq_numbers(src)))
